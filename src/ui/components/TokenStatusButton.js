fcc1007505a361d746e45fc06a8a64436187738a
Hamza MHIRA <mhirahamza@gmail.com>
2017-06-24 15:10:33 +0100

    fake claim it

--- src/ui/components/TokenStatusButton.js
+++ src/ui/components/TokenStatusButton.js
@@ -2,33 +2,74 @@ import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import ActionDone from 'material-ui/svg-icons/action/done';
-const TokenStatusButton = (props) => {
- 
+import CircularProgress from 'material-ui/CircularProgress';
 
-  if(props.claimed){
-    return (
-      <FlatButton
-          onClick={props.onClick}
-          label="Claimed"
-          labelStyle={{color:'#6EBF1D', textTransform: 'none'}}
-          style={{height:28}}
-          icon={<ActionDone style={{fill:"#6EBF1D", marginLeft:0}}/>}
-          disabled={true}
-        />
-      );
-  }else{
-    return (
-      <RaisedButton
-            onClick={props.onClick}
-            label="Claim it"
-            backgroundColor='#6EBF1D'
-            labelColor='#FFF'
-            labelStyle={{textTransform: 'none'}}
-            style={{height:28}}
-            buttonStyle={{lineHeight:'28px'}}
-          />
-    );
+class TokenStatusButton extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      claimed:props.claimed,
+      pending:false
+    }
+    this.claimCertificate = this.claimCertificate.bind(this);
   }
+
+  claimCertificate(){
+    this.setState({
+      claimed: true
+    })
+  }
+
+  render(){
+    const {claimed, pending} = this.state;
+
+    if(claimed){
+      return (
+        <FlatButton
+            label="Claimed"
+            labelStyle={{color:'#6EBF1D', textTransform: 'none'}}
+            style={{height:28}}
+            icon={<ActionDone style={{fill:"#6EBF1D", marginLeft:0}}/>}
+            disabled={true}
+          />
+        );
+    }else{
+      return (
+        <RaisedButton
+              onClick={() => {
+                let self = this
+                this.setState({
+                  pending:true
+                });
+                setTimeout(function(){ 
+                    self.claimCertificate()
+                }, 3000);
+              }}
+              label={pending?"":"Claim it"}
+              backgroundColor={pending?'rgba(110, 191, 29, 0.48)':'#6EBF1D'}
+              labelColor='#FFF'
+              labelStyle={{textTransform: 'none'}}
+              style={{height:28}}
+              buttonStyle={{lineHeight:'28px'}}
+            >
+             {pending ? (
+               <CircularProgress 
+                size={24} 
+                thickness={3} 
+                color="#FFF"
+                style={{
+                    position: "absolute",
+                    left: "35%",
+                    top: "2px"
+                }}
+              />
+             ): null}
+              
+            </RaisedButton>
+      );
+    }
+  }
+    
  
 };
 

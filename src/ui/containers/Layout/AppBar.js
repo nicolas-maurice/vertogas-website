40c635f2a1a9aa5a22d66ee7ac45540f29dc52e5
Hamza Lhioui <hamzalhioui@gmail.com>
2017-06-29 18:18:48 +0100

    add plus button

--- src/ui/containers/Layout/AppBar.js
+++ src/ui/containers/Layout/AppBar.js
@@ -1,16 +1,32 @@
 import React, { 
   PropTypes,
 } from 'react';
+import IconButton from 'material-ui/IconButton';
+import Add from 'material-ui/svg-icons/content/add';
 
 import './AppBar.css';
 
 import Logo from '../../../../public/img/logo.png';
-
+const styles = {
+  mediumIcon: {
+    width: 48,
+    height: 48,
+    
+  },
+  medium: {
+    float:'right',
+    right:30
+  }
+};
 
 const AppBar = (props) => {
   return (
      <div className="topBar">
         <img src={Logo} alt="Gasuni logo"/>
+        <IconButton iconStyle={styles.mediumIcon}
+                    style={styles.medium}>
+          <Add color='#FFF'/>
+        </IconButton>
      </div>
   );
 }
